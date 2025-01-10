d3112989af7c6b6fcaccd4b7106d5fbb1aa151ae
Ubuntu <ubuntu@ip-172-31-16-183.ec2.internal>
2025-01-10 21:03:25 +0000

    add module dir

---
+++ Module/vpc/main.tf
@@ -0,0 +1,8 @@
+# Create the VPC
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
+  }
+}
---
+++ Module/vpc/variable.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr" {
+  description = "vpc create through terraform"
+  type        = string
+}
+
+variable "vpc_name" {
+  type = string
+}
